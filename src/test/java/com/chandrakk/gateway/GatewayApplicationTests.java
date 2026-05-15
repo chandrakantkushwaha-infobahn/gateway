11926fe8376ad80c78747a575a46e2a0f80ebbcb
chandrakantkushwaha-infobahn <chandrakantkushwaha61@gmail.com>
2026-05-15 17:31:40 +0530

    gateway service

---
+++ src/test/java/com/chandrakk/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandrakk.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
